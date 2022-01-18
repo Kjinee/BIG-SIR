ee11186cf69205912ad0fadc978d56c80d60af31
Kjinee <96831813+Kjinee@users.noreply.github.com>
2022-01-18 20:43:24 +0900

    REFACTOR: Simplify function "dist"

--- 4-2-1_simulation1.R
+++ 4-2-1_simulation1.R
@@ -15,13 +15,8 @@ set.seed(1234)
 dist <- function(v1, v2) {
   v1 <- as.matrix(v1)
   v2 <- as.matrix(v2)
-  if (dim(v1)[2] == 1) {
-    p1 <- v1 %*% t(v1) / c(t(v1)%*%v1)
-    p2 <- v2 %*% t(v2) / c(t(v2)%*%v2)
-  } else {
-    p1 <- v1 %*% matpower(t(v1)%*%v1, -1) %*% t(v1)
-    p2 <- v2 %*% matpower(t(v2)%*%v2, -1) %*% t(v2)
-  }
+  p1 <- v1 %*% t(v1) / c(t(v1)%*%v1)
+  p2 <- v2 %*% t(v2) / c(t(v2)%*%v2)
   d <- sqrt( sum((p1-p2)*(p1-p2)) )
   return(d)
 }
