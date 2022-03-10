a6cca7f9e9a4b9246a97dd03fbb52381ea273cb1
Kjinee <96831813+Kjinee@users.noreply.github.com>
2022-03-11 00:47:57 +0900

    Update and rename boxplot_SAVE.R to boxplot.R

--- boxplot.R
+++ boxplot.R
@@ -66,17 +66,17 @@ BIG_SAVE <- function(x,y,k,ng,p=10){
 ######################################################################
 # function to generate data
 
-# (model 1) y = 2 (X'b1)^2 + e
-# (model 2) y = (X'b1)^2 + (X'b2)^2 + e
-# (model 3) y = sin(X'b1) + e
-# (model 4) y = sin(X'b1) + (x'b2)^2 + e
-# (model 5) y = 0.5 + X'b1^3 + e
-# (model 6) y = X'b1 / (0.5 + X'b2^2) + e
+# (model 1) y = (x'b1) + e
+# (model 2) y = 2 (X'b1)^2 + e
+# (model 3) y = (X'b1)^2 + (X'b2)^2 + e
+# (model 4) y = 0.5 + (x'b1)^3 + e
+# (model 5) y = exp(x'b1) + e
+# (model 6) y = X'b1 / (0.5 + X'b2^2) + e x'b1
 ######################################################################
 
-gen.xy <- function(model, n, p=10){
-  beta <- cbind(c(1,-1,2,-2,0,0,0,0,0,0),
-                     c(0,0,0,0,0,0,1,-1,2,-2))/sqrt(10)
+gen.xy <- function(model, n, p){
+  beta <- cbind(c(1,-1,2,-2,rep(0,p-4)),
+                c(rep(0,p-4),1,-1,2,-2))/sqrt(10)
   
   sig <- sqrt(2)
   eps <- rnorm(n, 0, sig^2)
@@ -86,46 +86,44 @@ gen.xy <- function(model, n, p=10){
   x <- rmvnorm(n, mean=rep(0,p), sigma=SIG)
   
   if (model==1){
-    y <- 2 * (x %*% beta[,1])^2 + eps ; k <- 1
+    y <- x %*% beta[,1] + eps ; k <- 1
   } else if (model==2){
-    y <- (x %*% beta[,1])^2 + (x %*% beta[,2])^2 + eps ; k <- 2
+    y <- 2 * (x %*% beta[,1])^2 + eps ; k <- 1
   } else if (model==3){
-    y <- sin(x %*% beta[,1]) + eps ; k <- 1
+    y <- (x %*% beta[,1])^2 + (x %*% beta[,2])^2 + eps ; k <- 2
   } else if (model==4){
-    y <- sin(x %*% beta[,1]) + (x %*% beta[,2])^2 + eps ; k <- 2
-  } else if (model==5){
     y <- 0.5 + (x %*% beta[,1])^3 + eps ; k <- 1
+  } else if (model==5){
+    y <- exp(x %*% beta[,1]) + eps ; k <- 1
   } else if (model==6){
-    y <- (x %*% beta[,1]) / (0.5 + (x %*% beta[,2])^2) + eps ; k <- 2
+    y <- x %*% beta[,1] / (0.5 + (x %*% beta[,2])^2) + eps * x %*% beta[,1] ; k <- 2
   }
   
   return(list(x=x, y=y, k=k, beta=beta[,1:k]))
 }
 
 ######################################################################
-# function to draw box plot to compare SAVE & BIG-SAVE for each model
+# function to compare SAVE & BIG-SAVE for each model
 ######################################################################
 
-draw.boxplot <- function(model){
+# calculate quality measure for each n & ng
+compare.qual <- function(model, p, n.list){
   set.seed(1234)
-  
   qual.list <- c()
-  n.list <- c(10^3, 5*10^3, 10^4, 5*10^4)
   
+  # 500 repetitions for each n
   for (n in n.list){
     if (n == 10^3){
       ng.list <- c(1, 10, 20)
     } else {
       ng.list <- c(1, 10, 50, 100)
     }
-    
-    # 500 repetitions
     for (m in 1:500){
-      data <- gen.xy(model, n)
+      data <- gen.xy(model, n, p)
       x <- data$x ; y <- data$y ; k <- data$k ; true.beta <- data$beta
       # calculate the estimator & measure the quality
       for (ng in ng.list){
-        beta <- BIG_SAVE(x,y,k,ng)
+        beta <- BIG_SAVE(x,y,k,ng,p)
         qual <- dist(beta, true.beta)
         qual.list <- rbind(qual.list, c(n,ng,qual))
       }
@@ -139,15 +137,38 @@ draw.boxplot <- function(model){
            quality = as.numeric(quality))
   qual.list$ng <- recode_factor(qual.list$ng, "1"="SAVE")
   
+  return(qual.list)
+}
+
+# draw boxplot of quality measure
+draw.boxplot <- function(model, qual.list) {
   ggplot(qual.list, aes(x=ng, y=quality)) +
     geom_boxplot(aes(group=ng)) +
     facet_wrap(~ n, ncol=4, scales = "free_x") +
     labs(title=paste("BIG-SAVE: model",model), x="G", y="Quality measure")
 }
 
-draw.boxplot(1)
-draw.boxplot(2)
-draw.boxplot(3)
-draw.boxplot(4)
-draw.boxplot(5)
-draw.boxplot(6)
+
+n.list <- c(10^4, 5*10^4, 10^5, 5*10^5)
+model1 <- compare.qual(model=1, p=10, n.list)
+draw.boxplot(1, model1)
+
+n.list <- c(10^3, 5*10^3, 10^4, 5*10^4)
+model2 <- compare.qual(model=2, p=20, n.list)
+draw.boxplot(2, model2)
+
+n.list <- c(5*10^3, 10^4, 5*10^4, 10^5)
+model3 <- compare.qual(model=3, p=25, n.list)
+draw.boxplot(3, model3)
+
+n.list <- c(5*10^3, 10^4, 5*10^4, 10^5)
+model4 <- compare.qual(model=4, p=15, n.list)
+draw.boxplot(4, model4)
+
+n.list <- c(5*10^3, 10^4, 5*10^4, 10^5)
+model5 <- compare.qual(model=5, p=15, n.list)
+draw.boxplot(5, model5)
+
+n.list <- c(10^5, 5*10^5, 10^6, 3*10^6)
+model6 <- compare.qual(model=6, p=10, n.list)
+draw.boxplot(6, model6)
